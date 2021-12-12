4e95a10375f719b9a59cd444b3d1d96d355855fa
Bakhtiyar Zubairov <irookanji@gmail.com>
2021-12-12 19:39:32 +0100

    add carousel images

--- src/components/collections/Collections.jsx
+++ src/components/collections/Collections.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import './collections.css';
+import React from "react";
+import "./collections.css";
+
+import image1 from "../../assets/Carousel_1_1.png";
+import image2 from "../../assets/Carousel_1_2.png";
+import image3 from "../../assets/Carousel_1_3.png";
+
+const images = [image1, image2, image3];
 
 const Collections = () => {
-    return (
-        <div>
-            Carousel
-        </div>
-    )
-}
+  return <div>Carousel</div>;
+};
 
-export default Collections
+export default Collections;
