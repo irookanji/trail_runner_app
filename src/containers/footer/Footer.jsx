a9c1e9a041cceb960738482fb04122879aa4f104
Bakhtiyar Zubairov <irookanji@gmail.com>
2021-12-23 19:06:58 +0100

    Add footer

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,12 +1,245 @@
-import React from 'react';
-import './footer.css';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import Icon from "@mui/material/Icon";
+
+import { footerData } from "./FooterData";
 
 const Footer = () => {
-    return (
-        <div>
-            Footer
-        </div>
-    )
-}
+  return (
+    <AppBar
+      position="static"
+      sx={{
+        background: "#212A2F",
+        mt: "2rem",
+        pl: "54px",
+        pt: "72px",
+        pb: "90px",
+        color: "#FFFFFF",
+        minHeight: "821px",
+        width: "100%",
+      }}
+    >
+      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Grid item xs={12} md={3}>
+          <Typography
+            variant="h6"
+            color="inherit"
+            component="div"
+            sx={{
+              fontFamily: "Roboto",
+              fontStyle: "normal",
+              fontWeight: "normal",
+              fontSize: "14px",
+              lineHeight: "21px",
+              letterSpacing: "2px",
+              textTransform: "uppercase",
+              mb: "21px",
+            }}
+          >
+            Help
+          </Typography>
+          <Box
+            sx={{
+              fontFamily: "Roboto",
+              fontStyle: "normal",
+              fontWeight: "normal",
+              fontSize: "12px",
+              lineHeight: "12px",
+              letterSpacing: "0.5px",
+            }}
+          >
+            <Typography>1-888-963-8944</Typography>
+            <Typography>1-814-251-9966 (Text)</Typography>
+            <Typography>help@allbirds.com</Typography>
+            <Typography>Returns/Exchanges</Typography>
+            <Typography>FAQ/Contact Us</Typography>
+            <Typography>Afterpay</Typography>
+          </Box>
+        </Grid>
 
-export default Footer
+        <Grid item xs={12} md={3}>
+          <Typography
+            variant="h6"
+            color="inherit"
+            component="div"
+            sx={{
+              fontFamily: "Roboto",
+              fontStyle: "normal",
+              fontWeight: "normal",
+              fontSize: "14px",
+              lineHeight: "21px",
+              letterSpacing: "2px",
+              textTransform: "uppercase",
+              mb: "21px",
+            }}
+          >
+            Shop
+          </Typography>
+          <Box
+            sx={{
+              fontFamily: "Roboto",
+              fontStyle: "normal",
+              fontWeight: "normal",
+              fontSize: "12px",
+              lineHeight: "12px",
+              letterSpacing: "0.5px",
+            }}
+          >
+            <Typography>Men's Shoes</Typography>
+            <Typography>Women's Shoes</Typography>
+            <Typography>Men's Apparel</Typography>
+            <Typography>Women's Apparel</Typography>
+            <Typography>Socks</Typography>
+            <Typography>Gift Cards</Typography>
+            <Typography>Download the Allbirds App</Typography>
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={3}>
+          <Typography
+            variant="h6"
+            color="inherit"
+            component="div"
+            sx={{
+              fontFamily: "Roboto",
+              fontStyle: "normal",
+              fontWeight: "normal",
+              fontSize: "14px",
+              lineHeight: "21px",
+              letterSpacing: "2px",
+              textTransform: "uppercase",
+              mb: "21px",
+            }}
+          >
+            Company
+          </Typography>
+          <Box
+            sx={{
+              fontFamily: "Roboto",
+              fontStyle: "normal",
+              fontWeight: "normal",
+              fontSize: "12px",
+              lineHeight: "12px",
+              letterSpacing: "0.5px",
+              mb: "119px",
+            }}
+          >
+            <Typography>Our Stores</Typography>
+            <Typography>Our Story</Typography>
+            <Typography>Our Materials</Typography>
+            <Typography>Sustainability</Typography>
+            <Typography>Partnerships</Typography>
+            <Typography>Product Testing</Typography>
+            <Typography>Affiliates</Typography>
+            <Typography>Bulk Orders</Typography>
+            <Typography>Careers</Typography>
+            <Typography>Press</Typography>
+            <Typography>California Transparency Act</Typography>
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={3}>
+          <Typography
+            variant="h6"
+            color="inherit"
+            component="div"
+            sx={{
+              fontFamily: "Roboto",
+              fontStyle: "normal",
+              fontWeight: "normal",
+              fontSize: "14px",
+              lineHeight: "21px",
+              letterSpacing: "2px",
+              textTransform: "uppercase",
+            }}
+          ></Typography>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Typography
+            variant="h6"
+            color="inherit"
+            component="div"
+            sx={{
+              fontFamily: "Roboto",
+              fontStyle: "normal",
+              fontWeight: "normal",
+              fontSize: "14px",
+              lineHeight: "21px",
+              letterSpacing: "2px",
+              textTransform: "uppercase",
+              mb: "21px",
+            }}
+          >
+            Follow the Flock
+          </Typography>
+          <Typography
+            variant="h6"
+            color="inherit"
+            component="div"
+            sx={{
+              fontFamily: "Roboto",
+              fontStyle: "normal",
+              fontWeight: "normal",
+              fontSize: "12px",
+              lineHeight: "18px",
+              letterSpacing: "0.5px",
+              width: "293px",
+            }}
+          >
+            Exclusive offers, a heads up on new things, and sightings of
+            Allbirds in the wild. Oh, we have cute sheep, too. #weareallbirds
+          </Typography>
+        </Grid>
+
+        <Grid item xs={12} md={6}>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "flex-end",
+              mr: "10rem"
+            }}
+          >
+            {footerData.map((item) => (
+              <Icon
+                key={item.icon}
+                sx={{ display: "flex", mr: "1rem" }}
+                style={{ fontSize: 30 }}
+              >
+                <img
+                  src={`${item.icon}`}
+                  srcSet={`${item.icon}`}
+                  alt={item.title}
+                />
+              </Icon>
+            ))}
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={12}>
+          <Typography
+            variant="h6"
+            color="inherit"
+            component="div"
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              fontFamily: "Roboto",
+              fontStyle: "normal",
+              fontWeight: "normal",
+              fontSize: "12px",
+              lineHeight: "18px",
+              letterSpacing: "0.5px",
+            }}
+          >
+            © 2021 Allbirds, Inc. All Rights Reserved.Terms, Privacy &
+            Accessibility
+          </Typography>
+        </Grid>
+      </Grid>
+    </AppBar>
+  );
+};
+
+export default Footer;
---
+++ src/containers/footer/FooterData.jsx
@@ -0,0 +1,7 @@
+import japan from "../../assets/japan.svg";
+import korea from "../../assets/korea.svg";
+
+export const footerData = [
+  { title: "Japan Flag", icon: japan },
+  { title: "Korean Flag", icon: korea },
+];
