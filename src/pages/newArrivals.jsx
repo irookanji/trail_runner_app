8a624f4232b5d2bc09d38d59f9e8f4cb7e1894da
Bakhtiyar Zubairov <irookanji@gmail.com>
2021-12-29 13:42:15 +0100

    Add boilerplate code to new arrivals and women pages

---
+++ src/pages/newArrivals.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Box } from "@mui/material"
+
+const NewArrivals = () => {
+    return (
+        <Box>
+            New Arrivals Goods
+        </Box>
+    )
+}
+
+export default NewArrivals
