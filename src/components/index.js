52c4410fa86704dbb80aa74248d52af6284dfdd5
Bakhtiyar Zubairov <irookanji@gmail.com>
2022-01-06 21:35:33 +0100

    refactor components - index file

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-export { default as Collections } from "./collections/Collections";
+// export { default as Collections } from "./collections/Collections";
 export { default as Favorites } from "./favorites/Favorites";
 export { default as NavbarMUI } from "./navbar/NavbarMUI";
 export { default as NewArrivals } from "./new_arrivals/NewArrivals";
