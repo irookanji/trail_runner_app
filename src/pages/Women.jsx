6169fc06956c6c1b6aa3a7e7c2d8ed8af8f7bbb7
Bakhtiyar Zubairov <irookanji@gmail.com>
2022-02-09 19:58:27 +0100

    Add filter by Material for Women products

--- src/pages/Women.jsx
+++ src/pages/Women.jsx
@@ -1,10 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import Cart from '../components/shoppingCart/card/Card';
+import ProductFilters from '../components/molecules/ProductFilters/ProductFilters';
 import { Box, CircularProgress } from '@mui/material';
 import { getProducts } from '../requests';
+import styled from 'styled-components';
+import theme from '../theme';
+
+export const Container = styled(Box)`
+  display: flex;
+  ${theme.breakpoints.up('xs')} {
+    flex-direction: column;
+  }
+  ${theme.breakpoints.up('md')} {
+    flex-direction: row;
+    align-items: flex-start;
+  }
+`;
+
+export const Loader = styled(CircularProgress)`
+  margin: 100px auto;
+  color: inherit;
+`;
 
 const Women = () => {
   const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
 
   const getAllProducts = async () => {
     const response = await getProducts();
@@ -18,25 +38,21 @@ const Women = () => {
     getAllProducts();
   }, []);
 
+  const handleFiltering = (filteredProducts) => {
+    setFilteredProducts(filteredProducts);
+  };
+
   return (
-    <Box sx={{ display: 'flex' }}>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          width: '15%',
-          position: 'fixed',
-          top: '12vh',
-        }}
-      ></Box>
-      {products.length !== 0 ? (
+    <Container>
+      <ProductFilters products={products} handleFiltering={handleFiltering} />
+      {filteredProducts.length !== 0 ? (
+        <Cart cartData={filteredProducts} />
+      ) : products.length !== 0 ? (
         <Cart cartData={products} />
       ) : (
-        <CircularProgress sx={{ margin: '100px auto' }} color="inherit" />
+        <Loader />
       )}
-    </Box>
+    </Container>
   );
 };
 
